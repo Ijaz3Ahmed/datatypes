322c1a302167272389092d97ff5692b7c681846c
Ijaz3Ahmed <146079789+Ijaz3Ahmed@users.noreply.github.com>
2023-09-28 21:16:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+function image1(){
+    
+    var ab="details";
+    if(true)
+    {
+        document.getElementById("taj1").textContent=ab;
+        ab="a.Architect by Ustaad Ahmed Lahori";
+        document.getElementById("taj2").textContent=ab;
+        ab="b.It was constructed in 1632";
+        document.getElementById("taj3").textContent=ab;
+        ab="c.It is located in Agra";
+        
+    }
+    document.getElementById("taj4").textContent=ab;
+   }
+
+function image2(){
+    
+    let ac="details";
+    if(true)
+    {
+        document.getElementById("char1").textContent=ac;
+        ac="a. Charminar was built by Sultan Mohammed Quli Qutb Shah in 1591.";
+        document.getElementById("char2").textContent=ac;
+        ac="b.  It is said that this square shaped structure with four pillars.  ";
+        document.getElementById("char3").textContent=ac;
+        ac="c.intricate carvings was built in honour of his wife, Bhagmati.";
+        
+        document.getElementById("char4").textContent=ac
+ 
+    }
+    
+   }
+function image3(){
+ let ad="details";
+    if(true)
+    {
+        document.getElementById("GATE1").textContent=ad;
+        ad="a.Architect: George Wittet ";
+        document.getElementById("GATE2").textContent=ad;
+        ad="b.Construction started:  ";
+        document.getElementById("GATE3").textContent=ad;
+        ad="c. 31 March 1911.";
+    document.getElementById("GATE4").textContent=ad ;  
+
+    }
+}
+   function image4(){
+var   ae="details";
+    if(true)
+    {
+        document.getElementById("MAHAL1").textContent=ae;
+        ae="a.Architect: George Wittet ";
+        document.getElementById("MAHAL2").textContent=ae;
+        ae="b.Construction started:  ";
+        document.getElementById("MAHAL3").textContent=ae;
+        ae="c. 31 March 1911.";
+       }
+       document.getElementById("MAHAL4").textContent=ae ; 
+    }
+       
+function image5(){
+ let a="details";
+    if(true)
+    {
+        document.getElementById("RED1").textContent=a;
+        a="a.Architect: George Wittet ";
+        document.getElementById("RED2").textContent=a;
+        a="b.Construction started:  ";
+        document.getElementById("RED3").textContent=a;
+        a="c. 31 March 1911.";
+    document.getElementById("RED4").textContent=ae ;  
+
+    }
+       
+   }
